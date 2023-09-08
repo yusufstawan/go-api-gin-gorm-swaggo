2d0531d26f2d6b287d065bd6f4107c0a7b4ac959
yusufstawan <yusufsetiyawan45@gmail.com>
2023-09-09 02:12:20 +0700

    update setting database

--- config/db.go
+++ config/db.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"api-gin/models"
+	"api-gin/utils"
 	"fmt"
 
 	"gorm.io/driver/mysql"
@@ -9,19 +10,20 @@ import (
 )
 
 func ConnectDatabase() *gorm.DB {
-	username := "yusufstawan"
-	password := "123456"
-	host := "tcp(127.0.0.1:3306)"
-	database := "database_movie"
+	username := utils.Getenv("DATABASE_USERNAME", "root")
+	password := utils.Getenv("DATABASE_PASSWORD", "DeNVPix1ZZsXPOFNRp6O")
+	host := utils.Getenv("DATABASE_HOST", "containers-us-west-52.railway.app")
+	port := utils.Getenv("DATABASE_PORT", "5471")
+	database := utils.Getenv("DATABASE_NAME", "railway")
 
-	dsn := fmt.Sprintf("%v:%v@%v/%v?charset=utf8mb4&parseTime=True&loc=Local", username, password, host, database)
+	dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=utf8mb4&parseTime=True&loc=Local", username, password, host, port, database)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		panic(err.Error())
 	}
 
-	db.AutoMigrate(&models.Movie{}, &models.AgeRatingCategory{}, &models.User{})
+	db.AutoMigrate(&models.User{}, &models.Movie{}, &models.AgeRatingCategory{})
 
 	return db
 }
